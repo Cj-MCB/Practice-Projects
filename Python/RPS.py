c26153e8e345507663c2cbb03da1ece877f772f8
Cj-MCB <carsonjclegg@gmail.com>
2024-04-11 12:51:52 -0600

    BeyondRound1

--- Python/RPS.py
+++ Python/RPS.py
@@ -1,15 +1,22 @@
 import random, time
 
-
 print("Welcome to Rock Paper Scissors!")
 
 
 def askplay():
+    if 'play' not in globals():
+        global play
+    else:
+        ...
     play = input("Are you ready to begin? (Yes/No)\n").lower()
-    return play
-
+    playchecker(play)
 
 def game():
+    global computerpoints
+    computerpoints = 0
+    global playerpoints
+    playerpoints = 0
+
     def rpsshoot():
         wait = time.sleep(1)
         print("Rock..")
@@ -34,34 +41,67 @@ def game():
 
     def detrandomchoice():
         intrandomchoice = random.randint(1, 3)
-        print(f"Random choice: {intrandomchoice}")
-        # Temporary Line to test tie checker
-        # intrandomchoice = int(1)
+        #print(f"Random choice: {intrandomchoice}")
         # 1 = R, 2 = P, 3 = S
         return intrandomchoice
 
     def tiechecker(choice):
-        print("Running tie-checker")
+        #print("Running tie-checker")
         randomchoice = detrandomchoice()
         if choice == randomchoice:
-            print(
-                f"Duplicate detected, your choice: {choice}, computer's choice: {randomchoice}"
-            )
-            # randomchoice = 0
+            #print(f"Duplicate detected, your choice: {choice}, computer's choice: {randomchoice}")
             tiechecker(choice)
         elif choice != randomchoice:
-            print(f"Your choice: {choice}, Computer's choice: {randomchoice}")
+            #print(f"Your choice: {choice}, Computer's choice: {randomchoice}")
+            return randomchoice
 
     choice = askchoice()
-    tiechecker(choice)
+    randomchoice = tiechecker(choice)
 
+    def endgamechecker(computerpoints, playerpoints):
+        if computerpoints >= 2:
+            ...
+        elif playerpoints >=2:
+            ...
+        else:
+            # Add round numbers later
+            print("Here comes the next round!")
+            askchoice()
 
-play = askplay()
-if play == "yes" or play == "y":
-    game()
-elif play == "no" or play == "n":
-    ...
-else:
-    ...
+    def resultchecker(computerpoints, playerpoints):
+        if choice == 1: #Rock
+            if randomchoice == 2: #Paper
+                computerpoints += 1
+                print(f"Paper beats rock, Computer wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+            elif randomchoice == 3: #Scissors
+                playerpoints += 1
+                print(f"Rock beats scissors, Player wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+        elif choice == 2: #Paper
+            if randomchoice == 1: #Rock
+                playerpoints += 1
+                print(f"Paper beats rock, Player wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+            elif randomchoice == 3: #Scissors
+                computerpoints += 1
+                print(f"Scissors beats paper, Computer wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+        elif choice == 3: #Scissors
+            if randomchoice == 1: #Rock
+                computerpoints += 1
+                print(f"Rock beats scissors, Computer wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+            elif randomchoice == 2: #Paper
+                playerpoints += 1
+                print(f"Scissors beats paper, Player wins!\nComputer: {computerpoints}\nPlayer: {playerpoints}")
+        endgamechecker(computerpoints, playerpoints)
 
-# Add results, points, no tie, etc.
+    resultchecker(computerpoints, playerpoints)
+    
+def playchecker(play):
+    if play == "yes" or play == "y":
+        game()
+    elif play == "no" or play == "n":
+        ...
+    else:
+        print(f"That was not an option!")
+        askplay()
+
+askplay()
+# Add results, etc.
